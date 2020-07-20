7a54841932b125454ece20654de9b74943c8dff3
Aidan Grabe <aidan@teamwork.com>
2020-07-20 17:35:15 +0100

    Send message about branch that was merged

--- index.js
+++ index.js
@@ -2,40 +2,69 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const request = require('request');
 
-try {
-  const slackWebhookUrl = core.getInput('SLACK_WEBHOOK');
-  const message = core.getInput('MESSAGE');
+const BASE_BRANCH_NAME = "develop";
+const FEATURE_BRANCH_PREFIX = "feature/";
 
-  if (!slackWebhookUrl || !message) {
-      console.log('Invalid input');
+function isMergedPr() {
+  const payload = github.context.payload;
+  return payload.action == "closed" && payload.pull_request.merged;
+}
+
+function getPrBranchName() {
+  const payload = github.context.payload;
+  return payload.pull_request.head.ref;
+}
+
+function isFeatureBranch() {
+  const payload = github.context.payload;
+  const headBranchName = getPrBranchName();
+  const baseBranchName = payload.pull_request.base.ref;
+
+  return baseBranchName.startsWith(FEATURE_BRANCH_PREFIX) && headBranchName == BASE_BRANCH_NAME;
+}
+
+function logPayload() {
+  const webHookPayload = github.context.payload;
+  console.log("Payload:");
+  console.log(JSON.stringify(webHookPayload));
+}
+
+function sendChatMessage(hookUrl, message) {
+  const options = {
+    url: hookUrl,
+    json: true,
+    body: {
+      body: message
+    }
+  };
+
+  console.log("Posting message: '" + message + "'");
+  console.log(JSON.stringify(options));
+
+  request.post(options, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(`Status: ${res.statusCode}`);
+    console.log(body);
+  });
+}
+
+try {
+  // TODO rename from Slack :flushed:
+  const chatHookUrl = core.getInput('SLACK_WEBHOOK');
+
+  if (!chatHookUrl || !message) {
+    console.log('Invalid input');
   } else {
 
-    // blah test
+    logPayload();
 
-    const webHookPayload = github.context.payload;
-    //const messageToSend = "";
+    if (isMergedPr() && isFeatureBranch()) {
+      const message = `:twisted_rightwards_arrows: Feature branch \`${getPrBranchName()}\` merged into \`${BASE_BRANCH_NAME}\``;
+      sendChatMessage(chatHookUrl, message);
+    }
 
-    console.log("Payload:");
-    console.log(JSON.stringify(webHookPayload));
-
-    const options = {
-        url: slackWebhookUrl,
-        json: true,
-        body: {
-            body: message
-        }
-    };
-
-    console.log("Posting message: '" + message + "'");
-    console.log(JSON.stringify(options));
-    
-    request.post(options, (err, res, body) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log(`Status: ${res.statusCode}`);
-        console.log(body);
-    });
   }
 } catch (error) {
   core.setFailed(error.message);
